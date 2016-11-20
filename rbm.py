5914638158f19127ff0e5ea21212eed893592b2a
patricieni <patric.fulop@gmail.com>
2016-11-20 00:45:30 -0800

    Add more comments

--- rbm.py
+++ rbm.py
@@ -233,21 +233,6 @@ if __name__ == '__main__':
     err = rbm.run_contrastive_divergence(K=1, data=data, epochs=3000)
     error_plot(err, 3000)
 
-    input_data = np.array([1, 0.5, 0.5, 0.5,
-                           0, 0.5, 0.5, 0.5,
-                           1, 0.5, 0.5, 0.5,
-                           0, 0.5, 0.5, 0.5])
-    input_data_1 = np.array([0.5, 0.5, 0.5, 0.5,
-                             0.5, 1, 1, 0.5,
-                             0.5, 1, 1, 0.5,
-                             0.5, 0.5, 0.5, 0.5
-                             ])
-    input_data_2 = np.array([0.5, 0.5, 0, 0.5,
-                             0.5, 0.5, 1, 0.5,
-                             0.5, 0.5, 1, 0.5,
-                             0.5, 0.5, 0, 0.5
-                             ])
-
     input_data_test = np.array([1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0])
     input_data_1_test = np.array([1, 1, 1, 1,
                                   1, 1, 1, 1,
@@ -261,8 +246,13 @@ if __name__ == '__main__':
     c1 = 0
     c1_1 = 0
     for i in range(0, 10000):
-        reconstructed_data = rbm.run_gibbs(1, input_data)
-        reconstructed = rbm.daydream(1, input_data)
+        input_data = np.array([1, 0.5, 0.5, 0.5,
+                               0, 0.5, 0.5, 0.5,
+                               1, 0.5, 0.5, 0.5,
+                               0, 0.5, 0.5, 0.5])
+
+        reconstructed_data = rbm.run_gibbs(4, input_data)
+        reconstructed = rbm.daydream(4, input_data)
 
         if i > 1000:
             if np.sum((reconstructed_data - input_data_test) ** 2) == 0:
@@ -277,8 +267,14 @@ if __name__ == '__main__':
     c2 = 0
     c2_2 = 0
     for j in range(0, 10000):
-        rec_data = rbm.run_gibbs(1, input_data_1)
-        rec_data1 = rbm.daydream(1, input_data_1)
+        input_data_1 = np.array([0.5, 0.5, 0.5, 0.5,
+                                 0.5, 1, 1, 0.5,
+                                 0.5, 1, 1, 0.5,
+                                 0.5, 0.5, 0.5, 0.5
+                                 ])
+
+        rec_data = rbm.run_gibbs(4, input_data_1)
+        rec_data1 = rbm.daydream(4, input_data_1)
 
         if j > 1000:
             if np.sum((rec_data - input_data_1_test) ** 2) == 0:
@@ -287,14 +283,20 @@ if __name__ == '__main__':
                 c2_2 += 1
         j += 1
     print("Ex 2: Reconstructed Data from last step: ", rec_data)
-    print("Ex 2: Probability to get matrix of ones. There are other possibilities as well. ", c2/9000, c2_2/9000)
+    print("Ex 2: Probability to get matrix of ones. There are other possibilities as well. 8 to be more precise. 12 % ", c2/9000, c2_2/9000)
 
     m = 0
     c3 = 0
     c3_3 = 0
     for m in range(0, 10000):
-        rec_data2 = rbm.run_gibbs(1, input_data_2)
-        rec_data2_2 = rbm.daydream(1, input_data_2)
+        input_data_2 = np.array([0.5, 0.5, 0, 0.5,
+                                 0.5, 0.5, 1, 0.5,
+                                 0.5, 0.5, 1, 0.5,
+                                 0.5, 0.5, 0, 0.5
+                                 ])
+
+        rec_data2 = rbm.run_gibbs(4, input_data_2)
+        rec_data2_2 = rbm.daydream(4, input_data_2)
 
         if m > 1000:
             if np.sum((rec_data2 - input_data_2_test) ** 2) == 0:
